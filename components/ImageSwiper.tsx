7341b5e3001dd095a86ef6233c2222d01f920ded
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2025-04-10 21:42:45 +0100

    add arrow to gallery

--- components/ImageSwiper.tsx
+++ components/ImageSwiper.tsx
@@ -1,17 +1,16 @@
 "use client";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 import {
-    CarouselSliceDefaultPrimaryImagesItem,
-    Simplify,
+  CarouselSliceDefaultPrimaryImagesItem,
+  Simplify,
 } from "@/prismicio-types";
 import { staticBlurDataUrl } from "@/utils/staticBlurUrl";
 import { GroupField } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import { useRef } from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 type SlideImageProps = {
   data: GroupField<Simplify<CarouselSliceDefaultPrimaryImagesItem>>;
@@ -24,7 +23,7 @@ export function ImageSwiper({ data }: SlideImageProps) {
 
   return (
     <Swiper
-      modules={[Pagination]}
+      modules={[Navigation, Pagination]}
       grabCursor={true}
       initialSlide={3}
       centeredSlides={true}
@@ -42,6 +41,11 @@ export function ImageSwiper({ data }: SlideImageProps) {
       onSwiper={(swiper) => {
         swiperWrappedRef.current = swiper.wrapperEl;
       }}
+      navigation={{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }}
+      className="mySwiper"
     >
       {data.map((slide, index) => (
         <SwiperSlide
@@ -66,6 +70,54 @@ export function ImageSwiper({ data }: SlideImageProps) {
           />
         </SwiperSlide>
       ))}
+
+      {/* arrow navigation */}
+      <div className="relative bottom-16 flex items-center justify-center gap-x-[10px] mt-8">
+        <div className="swiper-button-prev after:content-none relative h-10 w-10 mr-8">
+          <button
+            type="button"
+            className="z-40 h-10 w-10 rounded-full bg-white flex items-center justify-center disabled:opacity-50"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-arrow-left-icon lucide-arrow-left"
+            >
+              <path d="m12 19-7-7 7-7" />
+              <path d="M19 12H5" />
+            </svg>
+          </button>
+        </div>
+        <div className="swiper-button-next after:content-none relative h-10 w-10">
+          <button
+            type="button"
+            className="z-40 h-10 w-10 rounded-full bg-white flex items-center justify-center disabled:opacity-50"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-arrow-right-icon lucide-arrow-right"
+            >
+              <path d="M5 12h14" />
+              <path d="m12 5 7 7-7 7" />
+            </svg>
+          </button>
+        </div>
+      </div>
     </Swiper>
   );
 }
